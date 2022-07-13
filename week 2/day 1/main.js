9a176a3d9792066318d4b703bb792b85f5acdfa6
Janhavi147 <vijaysingrajput76@gmail.com>
2022-07-13 11:46:22 +0530

    java script

---
+++ week 2/day 1/main.js
@@ -0,0 +1,13 @@
+let name
+function get() {
+
+
+    let username=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+    if((username=="Janhavi") &&(password=="1234"))
+    {
+        alert("Login Successfully")
+    }
+    else
+    alert("Login Failed")
+}
